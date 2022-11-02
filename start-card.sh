7990ed2ec8890ca441fd5af4729547f6c958433e
Swicegood <jguru108@gmail.com>
2022-11-01 21:27:00 -0400

    Update start-card.sh

--- start-card.sh
+++ start-card.sh
@@ -1,1 +1,15 @@
-.
+#!/bin/bash
+
+v4l2-ctl -d /dev/video1 --set-ctrl=video_gop_size=1 -p 15
+v4l2-ctl -d /dev/video3 --set-ctrl=video_gop_size=1 -p 15
+v4l2-ctl -d /dev/video4 --set-ctrl=video_gop_size=1 -p 15
+v4l2-ctl -d /dev/video5 --set-ctrl=video_gop_size=1 -p 15
+v4l2-ctl -d /dev/video7 --set-ctrl=video_gop_size=1 -p 15
+v4l2-ctl -v width=704,height=480  -d /dev/video1
+v4l2-ctl -v width=704,height=480  -d /dev/video3
+v4l2-ctl -v width=704,height=480  -d /dev/video4
+v4l2-ctl -v width=704,height=480  -d /dev/video5
+v4l2-ctl -v width=704,height=480  -d /dev/video7
+
+
+echo `date` " Startcard was run" `v4l2-ctl -d /dev/video7 --get-ctrl=video_gop_size` >> start_card.log
